04146f9f6747efe59dc148341ea9943e6bc86ec3
Julien Colombain <julien@colombain.com>
2020-11-01 13:45:03 +0100

    :tada: initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React, { ComponentElement } from 'react';
+import '../stylesheets/components/App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Julien Colombain's website</h1>
+        <p>In construction...</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"></a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
